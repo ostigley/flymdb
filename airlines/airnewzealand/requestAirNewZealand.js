1c1265ffe479823872b492dc832c5c84eb1cfdc3
Oliver Stigley <oliverstigley@gmail.com>
2016-05-11 20:24:25 +1200

    fix bug in request air nz file before merge

--- airlines/airnewzealand/requestAirNewZealand.js
+++ airlines/airnewzealand/requestAirNewZealand.js
@@ -1,40 +1,26 @@
 var monthList = require('../../data/month-strings.js')
 var getPage = require('../get-page.js')
 var urlsArray = require('./data/urls')
-var htmlArray = require('../../test/data/airNzhtmlArray.js')
 
 module.exports= function() {
-	 return new Promise ( function (resolve, reject) {
-		var htmlString = htmlArray.reduce(function(a,b) {
-					return a + b
-				},"")
-	 	resolve(htmlString)
-	 })
-		
-		// var urls = urlsArray();
-		// return Promise.all(urls.map(getPage))
-		// 	.then( function(htmlArray) {
-		// 		return htmlArray.reduce(function(a,b) {
+	// for TESTING!!!!
+	// var htmlArray = require('../../test/data/airNzhtmlArray.js')
+	 // return new Promise ( function (resolve, reject) {
+		// var htmlString = htmlArray.reduce(function(a,b) {
 		// 			return a + b
 		// 		},"")
-
-		// 	})
-	//  various links that need to be visited at air nz: 
-
+	 // 	resolve(htmlString)
+	 // })
+		console.log("Requesting air new zealand")
+		var urls = urlsArray();
+		return Promise.all(urls.map(getPage))
+			.then( function(htmlArray) {
+				return htmlArray.reduce(function(a,b) {
+					return a + b
+				},"")
+			})
 }
 	
-
-
-
-
-	// return Promise.all(urls.map(getPage))
-
-
-			
-
-
-
-
 
 
 
