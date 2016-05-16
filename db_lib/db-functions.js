eb9fe2af545ba8c7fca1cedc953c4beed9f3b228
Oliver Stigley <oliverstigley@gmail.com>
2016-05-16 17:59:05 +1200

    migrations working for sqlite

---
+++ db_lib/db-functions.js
@@ -0,0 +1,12 @@
+/*
+functions to write: 
+	get airline movies 
+	doesMovieExist - check if movie is already in movies table - takes title, returns true or false .
+	add movie
+
+
+
+
+
+
+*/
---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    client: 'sqlite3',
+    connection: {
+    filename: "./flymydb.sqlite"
+  },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'flymdbDB',
+      user:     'process.env.PGSQL_USERNAME',
+      password: 'process.env.PGSQL_PASSWORD'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'flymdbDB',
+      user:     'process.env.PGSQL_USERNAME',
+      password: 'process.env.PGSQL_PASSWORD'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
