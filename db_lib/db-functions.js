aba89d6aab4cd69adf652178718c9a1c041ef7b8
Oliver Stigley <oliverstigley@gmail.com>
2016-05-16 18:33:30 +1200

    draft db functions composedclear

--- db_lib/db-functions.js
+++ db_lib/db-functions.js
@@ -1,3 +1,4 @@
+var getMovie = require('../movie-data/get-movies.js')
 /*
 functions to write: 
 	get airline movies 
@@ -7,6 +8,41 @@ functions to write:
 
 
 
-
-
 */
+
+exports.airlineMovies = function (airline, callback) {
+	knex('movies')
+		.join(airline, 'movies.id', `${airline}.movieId`)
+		.then( function (resp) {
+			callback(null, resp)
+		})
+}
+
+exports.addMovieIfNotExist = function (title, airine, callback) {
+	knex('movies')
+		.where('Title', title)
+		.then( function (resp) {
+			if (resp.length === 0) {
+				getMovie([title])
+					.then(function (resp) {
+						knex('movies')
+							.insert(JSON.pars(resp))
+							.then(function (resp) {
+								knex(airline)
+									.insert({movieId: resp[0]})
+									.then( function (resp) {
+										console.log("New Movie Added: ", title)
+										callback(null, resp)
+									})
+							})
+					})
+			} else {
+				knex(airline)
+					.insert({movieId: resp[0].id})
+					.then( function (resp) {
+						console.log("Existing movie added to airline DB: ", title)
+						callback(null, resp)
+					})
+			}
+		})
+}
